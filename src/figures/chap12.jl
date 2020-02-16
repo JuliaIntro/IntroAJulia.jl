511a613c7063ada0f04efd6d6c5d5fbd9715509d
Pamela Bustamante <pebustamante@uc.cl>
2020-02-16 15:59:48 -0300

    Update chap12.jl

--- src/figures/chap12.jl
+++ src/figures/chap12.jl
@@ -2,9 +2,9 @@ function fig12_1(output::Symbol, font::String, scale::Float64)
   p = TikzPicture(L"""
   \node[draw, fill=mycolor, minimum width=3.5cm, minimum height=1cm] at(0,0){};
   \node[anchor=east] (a) at(-1.25, 0.25) {\tt 1};
-  \node[anchor=west] (av) at (-0.25, 0.25) {\tt "Cleese"};
+  \node[anchor=west] (av) at (-0.25, 0.25) {\tt "Cortázar"};
   \node[anchor=east] (b) at(-1.25, -0.25) {\tt 2};
-  \node[anchor=west] (bv) at (-0.25, -0.25) {\tt "John"};
+  \node[anchor=west] (bv) at (-0.25, -0.25) {\tt "Julio"};
   \draw[-latex] (a) -- (av);
   \draw[-latex] (b) -- (bv);
   """; options= "scale=$scale, transform shape", preamble="""
@@ -19,22 +19,22 @@ end
 function fig12_2(output::Symbol, font::String, scale::Float64)
   p = TikzPicture(L"""
   \node(hist) [draw, fill=mycolor, minimum width=7.5cm, minimum height=3cm]{};
-  \node[anchor=east](nc) at(-0.25,1.25) {\tt ("Cleese","John")};
+  \node[anchor=east](nc) at(-0.25,1.25) {\tt ("Cortázar","Julio")};
   \node[anchor=west](c) at(0.75,1.25) {\tt "08700 100 222"};
   \draw[-latex](nc)--(c);
-  \node[anchor=east](ng) at(-0.25,0.75) {\tt ("Chapman","Graham")};
+  \node[anchor=east](ng) at(-0.25,0.75) {\tt ("Mistral","Gabriela")};
   \node[anchor=west](g) at(0.75,0.75) {\tt "08700 100 222"};
   \draw[-latex](ng)--(g);
-  \node[anchor=east](ni) at(-0.25,0.25) {\tt ("Idle","Eric")};
+  \node[anchor=east](ni) at(-0.25,0.25) {\tt ("Vallejo","César")};
   \node[anchor=west](i) at(0.75,0.25) {\tt "08700 100 222"};
   \draw[-latex](ni)--(i);
-  \node[anchor=east](nt) at(-0.25,-0.25) {\tt ("Gilliam","Terry")};
+  \node[anchor=east](nt) at(-0.25,-0.25) {\tt ("Sáenz","Jaime")};
   \node[anchor=west](t) at(0.75,-0.25) {\tt "08700 100 222"};
   \draw[-latex](nt)--(t);
-  \node[anchor=east](nj) at(-0.25,-0.75) {\tt ("Jones","Terry")};
+  \node[anchor=east](nj) at(-0.25,-0.75) {\tt ("Asunción","José")};
   \node[anchor=west](j) at(0.75,-0.75) {\tt "08700 100 222"};
   \draw[-latex](nj)--(j);
-  \node[anchor=east](np) at(-0.25,-1.25) {\tt ("Palin","Michael")};
+  \node[anchor=east](np) at(-0.25,-1.25) {\tt ("Eloy","Andrés")};
   \node[anchor=west](p) at(0.75,-1.25) {\tt "08700 100 222"};
   \draw[-latex](np)--(p);
   """; options= "scale=$scale, transform shape", preamble="""
