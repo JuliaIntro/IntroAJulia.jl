537a3d940f92c64d16926d8a8f2b28173ec13ae9
Pamela Bustamante <pebustamante@uc.cl>
2020-02-16 15:37:09 -0300

    Update chap10.jl

--- src/figures/chap10.jl
+++ src/figures/chap10.jl
@@ -1,6 +1,6 @@
 function fig10_1(output::Symbol, font::String, scale::Float64)
   p = TikzPicture(L"""
-  \node[anchor=east](ch) at(-2.75, 0) {\tt cheeses};
+  \node[anchor=east](ch) at(-2.75, 0) {\tt quesos};
   \node[draw, fill=mycolor, minimum width=3.5cm, minimum height=1.5cm](chv){};
   \node[anchor=east] (ch1) at(-1.25, 0.5) {\tt 1};
   \node[anchor=west] (chv1) at (-0.25, 0.5) {\tt "Cheddar"};
@@ -12,7 +12,7 @@ function fig10_1(output::Symbol, font::String, scale::Float64)
   \draw[-latex] (ch1) -- (chv1);
   \draw[-latex] (ch2) -- (chv2);
   \draw[-latex] (ch3) -- (chv3);
-  \node[anchor=east](nu) at(-2.75, -1.75) {\tt numbers};
+  \node[anchor=east](nu) at(-2.75, -1.75) {\tt numeros};
   \node[draw, fill=mycolor, minimum width=2.5cm, minimum height=1.5cm](nuv) at(-0.5, -1.75){};
   \node[anchor=east] (nu1) at(-1.25, -1.25) {\tt 1};
   \node[anchor=west] (nuv1) at (-0.25, -1.25) {\tt 42};
@@ -23,7 +23,7 @@ function fig10_1(output::Symbol, font::String, scale::Float64)
   \draw[-latex] (nu1) -- (nuv1);
   \draw[-latex,dashed] (nu2) -- (nuv2);
   \draw[-latex] (nu2) -- (nuv3);
-  \node[anchor=east](em) at(-2.75, -3) {\tt empty};
+  \node[anchor=east](em) at(-2.75, -3) {\tt vacio};
   \node[draw, fill=mycolor, minimum width=0.5cm, minimum height=0.5cm](emv) at(-1.5, -3){};
   \draw[-latex] (em) -- (emv);
   """; options= "scale=$scale, transform shape", preamble="""
@@ -98,8 +98,8 @@ function fig10_5(output::Symbol, font::String, scale::Float64)
   p = TikzPicture(L"""
   \node[anchor=east] at(-1.2,0){\tt Main};
   \node[draw, fill=mycolor, minimum width=2cm, minimum height=0.5cm] at(0,0){};
-  \node[anchor=west] (l) at(-0.75, 0) {\tt letters};
-  \node[anchor=east] at(-1.2,-1){\tt deletehead!};
+  \node[anchor=west] (l) at(-0.75, 0) {\tt letras};
+  \node[anchor=east] at(-1.2,-1){\tt borrarprimero!};
   \node[draw, fill=mycolor, minimum width=2cm, minimum height=0.5cm] at(0,-1){};
   \node[anchor=west] (t) at(-0.75, -1) {\tt t};
   \node[draw, fill=mycolor, minimum width=2.5cm, minimum height=1.5cm] (a) at(3.25,-0.5){};
